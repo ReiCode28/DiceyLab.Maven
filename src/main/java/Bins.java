ebbdbe76b4faf4624e30eabe9bcbcbb021861424
randy <randyr_1993@yahoo.com>
2022-09-11 23:21:43 -0400

    Completed Bins and Dice classes

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bins {
+        private Map<Integer, Integer> mapOfBins = new HashMap<>();
 
+        public Bins(int lowestBinNumber, int highestBinNumber){
+            for(int i = lowestBinNumber; i <= highestBinNumber; i++){
+                mapOfBins.put(i, 0);
+            }
+        }
+
+        public Integer getBin(int binNumber){
+            return mapOfBins.get(binNumber);
+
+        }
+
+        public void incrementBin(int binNumber){
+            mapOfBins.replace(binNumber, mapOfBins.get(binNumber)+1);
+        }
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,42 @@
+import java.util.Random;
+
 public class Dice {
+
+    private Integer numberOfDice;
+    private Integer rollResult;
+    private Integer seed;
+    private Random random = new Random();
+    public Dice(int numberOfDice){
+        this.numberOfDice=numberOfDice;
+    }
+
+    public Dice(Integer numberOfDice, int seed){
+        this.numberOfDice = numberOfDice;
+        this.seed = seed;
+
+    }
+
+
+    public Integer tossAndSum(){
+        Integer rollSum = 0;
+        for(int i=0; i<numberOfDice; i++){
+            rollResult = random.nextInt(6) + 1;
+            rollSum += rollResult;
+
+        }
+        return rollSum;
+    }
+
+    public Integer tossAndSumSeed(){
+        Random random = new Random(seed);
+        Integer rollSum = 0;
+        for(int i=0; i<numberOfDice; i++){
+            rollResult = random.nextInt(6) + 1;
+            rollSum += rollResult;
+
+        }
+        return rollSum;
+    }
 
 
 }
