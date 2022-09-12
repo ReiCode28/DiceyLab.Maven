fb199cecd1c53cdf328af04dace815667593fa73
randy <randyr_1993@yahoo.com>
2022-09-11 23:47:09 -0400

    Completed Lab

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,41 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Dice {
 
+    private List<Integer> diceContainer = new ArrayList<Integer>();
     private Integer numberOfDice;
-    private Integer rollResult;
-    private Integer seed;
-    private Random random = new Random();
-    public Dice(int numberOfDice){
-        this.numberOfDice=numberOfDice;
-    }
+    private Integer sumOfDice;
 
-    public Dice(Integer numberOfDice, int seed){
+    public Dice(Integer numberOfDice){
         this.numberOfDice = numberOfDice;
-        this.seed = seed;
-
     }
 
+    public Dice(){
+        this.numberOfDice = 2;
+    }
+
+    public Integer getNumberOfDice(){
+        return this.numberOfDice;
+    }
+
+    public void setNumberOfDice(Integer numberOfDice){
+        this.numberOfDice = numberOfDice;
+    }
+
+    public List<Integer> getDiceList(){
+        return this.diceContainer;
+    }
+
+    public Integer getSumOfDice(){
+        return this.sumOfDice;
+    }
+
+    public void initializeDiceList(){
+        Integer numberOfDice = this.getNumberOfDice();
+        List<Integer> diceList = this.getDiceList();
+        for(int i = 0; i < numberOfDice; i++){
+            diceList.add(0);
+        }
+    }
+
+
+
+    public void tossDice(){
+        Integer min = 1;
+        Integer max = 7;
+        List<Integer> diceList = this.getDiceList();
+        Integer length = diceList.size();
+        for(int i = 0; i < length; i++){
+            diceList.set(i, (int)(Math.random() * (max - min)) + min);
+        }
+    }
 
     public Integer tossAndSum(){
-        Integer rollSum = 0;
-        for(int i=0; i<numberOfDice; i++){
-            rollResult = random.nextInt(6) + 1;
-            rollSum += rollResult;
+        Integer sum = 0;
+        this.tossDice();
+        List<Integer> diceList = this.getDiceList();
 
+        for(int i = 0; i < diceList.size(); i++){
+            sum += diceList.get(i);
         }
-        return rollSum;
+        this.sumOfDice = sum;
+        return sum;
     }
 
-    public Integer tossAndSumSeed(){
-        Random random = new Random(seed);
-        Integer rollSum = 0;
-        for(int i=0; i<numberOfDice; i++){
-            rollResult = random.nextInt(6) + 1;
-            rollSum += rollResult;
-
+    public void printDice(){
+        List<Integer> diceList = this.getDiceList();
+        for(int i = 0; i < diceList.size(); i++){
+            System.out.println((i + 1) + ": " + diceList.get(i));
         }
-        return rollSum;
     }
 
 
